9b0a4d7acfa3456f2264239a30aa62a3f7d203ac
ErnestasJankauskas <164503541+ErnestasJankauskas@users.noreply.github.com>
2024-05-10 15:30:42 +0300

    Update 1-data_prep.R

--- R/1-data_prep.R
+++ R/1-data_prep.R
@@ -1,16 +1,6 @@
 library(tidyverse)
 library(jsonlite)
-cat("Darbinė direktorija:", getwd())
 
-args <- commandArgs(trailingOnly = TRUE)
-
-if (length(args) > 0) {
-  file_format <- args[1]
-} else {
-  file_format <- "csv"
-}
-
-if (file_format == "csv"){
   download.file("https://atvira.sodra.lt/imones/downloads/2023/monthly-2023.csv.zip", "../data/temp")
   unzip("../data/temp",  exdir = "../data/")
   readLines("../data/monthly-2023.csv", 2)
@@ -21,15 +11,5 @@ if (file_format == "csv"){
     saveRDS("../data/kaunas15.rds")
   file.remove("../data/temp")
   file.remove("../data/monthly-2023.csv")
-} else {
-  download.file("https://atvira.sodra.lt/imones/downloads/2023/monthly-2023.json.zip", "../data/temp")
-  unzip("../data/temp",  exdir = "../data/")
-  readLines("../data/monthly-2023.json", 20)
-  data <- fromJSON("../data/monthly-2023.json")
-  data %>%
-    filter(`ecoActCode` == 451100) %>%
-    saveRDS("../data/kaunas.rds")
-  file.remove("../data/temp")
-  file.remove("../data/monthly-2023.json")
-}
+
 
